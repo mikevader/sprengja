2440dd5accdc2c612f8473925e018740ea0906c8
extscch1 <christian.schwald@zuehlke.com>
2014-06-30 13:49:03 +0200

    Fixed few typos

--- public/CoordinateModel.js
+++ public/CoordinateModel.js
@@ -1,85 +1,88 @@
-function CoordinateModel(var worldMin,var worldMax){
+/*global CustomEvent */
+'use strict';
+
+function CoordinateModel(worldMin, worldMax) {
     this.worldMin = worldMin;
     this.worldMax = worldMax;
     this.worldRange = worldMax - worldMin;
     this.screenSize = 0;
     this.isDeviceCoordinatesInverted = false;
     this.isWorldCoordinatesInverted = false;
-};
+}
 
-CoordinateModel.prototype.getScreenSize(){
+CoordinateModel.prototype.getScreenSize = function () {
     return this.screenSize;
 };
 
-CoordinateModel.prototype.setScreenSize(var screenSize){
+CoordinateModel.prototype.setScreenSize = function (screenSize) {
     this.screenSize = screenSize;
     this.fireModelChanged();
-}:
+};
 
-CoordinateModel.prototype.isDeviceCoordinatesInverted(){
+CoordinateModel.prototype.isDeviceCoordinatesInverted = function () {
     return this.isDeviceCoordinatesInverted;
 };
 
-CoordinateModel.prototype.setDeviceCoordinatesInverted(var isDeviceCoordinatesInverted){
+CoordinateModel.prototype.setDeviceCoordinatesInverted = function (isDeviceCoordinatesInverted) {
     this.isDeviceCoordinatesInverted = isDeviceCoordinatesInverted;
     this.fireModelChanged();
 };
 
-CoordinateModel.prototype.isWorldCoordinatesInverted(){
+CoordinateModel.prototype.isWorldCoordinatesInverted = function () {
     return this.isWorldCoordinatesInverted;
 };
 
-CoordinateModel.prototype.setWorldCoordinatesInverted(var worldCoordinatesInverted){
+CoordinateModel.prototype.setWorldCoordinatesInverted = function (worldCoordinatesInverted) {
     this.isWorldCoordinatesInverted = worldCoordinatesInverted;
     this.fireModelChanged();
 };
 
-CoordinateModel.prototype.getWorldMin(){
+CoordinateModel.prototype.getWorldMin = function () {
     return this.worldMin;
 };
 
-CoordinateModel.prototype.getWorldMax(){
+CoordinateModel.prototype.getWorldMax = function () {
     return this.worldMax;
 };
 
-CoordinateModel.prototype.setWorldRange(var worldMin,var worldMax){
+CoordinateModel.prototype.setWorldRange = function (worldMin, worldMax) {
     this.worldMin = worldMin;
     this.worldMax = worldMax;
     this.worldRange = worldMax - worldMin;
     this.fireModelChanged();
 };
 
-CoordinateModel.prototype.screenToNormalized(var screeValue){
+CoordinateModel.prototype.screenToNormalized = function (screenValue) {
     return (screenValue /  (this.screenSize - 1));
-}
+};
 
-CoordinateModel.prototype.screenToWorld(var screenValue){
+CoordinateModel.prototype.screenToWorld = function (screenValue) {
     var converted = this.screenToNormalized(screenValue);
     converted = this.checkOrientation(converted);
     return ((converted * this.worldRange) + this.worldMin);
 };
 
-CoordinateModel.prototype.worldToNormalized(var worldValue){
+CoordinateModel.prototype.worldToNormalized = function (worldValue) {
     return ((worldValue - this.worldMin) / this.worldRange);
 };
 
-CoordinateModel.prototype.worldToScreen(var worldValue){
+CoordinateModel.prototype.worldToScreen = function (worldValue) {
     var converted = this.worldToNormalized(worldValue);
     converted = this.checkOrientation(converted);
     return (converted * (this.screenSize - 1));
 };
 
-CoordinateModel.prototype.checkOrientation(var normalizedValue){
+CoordinateModel.prototype.checkOrientation = function (normalizedValue) {
     if (this.isDeviceCoordinatesInverted) {
-            normalizedValue = 1.0 - normalizedValue;
+        normalizedValue = 1.0 - normalizedValue;
     }
-    if (isWorldCoordinatesInverted) {
-            normalizedValue = 1.0 - normalizedValue;
+    if (this.isWorldCoordinatesInverted) {
+        normalizedValue = 1.0 - normalizedValue;
     }
     return normalizedValue;
 };
 
-CoordinateModel.prototype.fireModelChanged(){
-    CustomEvent event = new CustomEvent("coordinateModelChanged");
-    document.dispatchEvent(event);
-}
+CoordinateModel.prototype.fireModelChanged = function () {
+    var customEvent = new CustomEvent("coordinateModelChanged");
+    document.dispatchEvent(customEvent);
+};
