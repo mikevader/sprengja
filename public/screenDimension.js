35b059f3b83b7f74df6f14e6ed52912464b95239
Nicolas Oeschger <noe@exq.ch>
2014-06-30 14:19:50 +0200

    Make game fullscreen on start

--- public/screenDimension.js
+++ public/screenDimension.js
@@ -1,3 +1,21 @@
-/**
- * Created by nici on 30.06.14.
- */
+function ScreenDimension() {
+    this.width = getScreenWidth();
+    this.height = getScreenHeight();
+    assureLandscape();
+
+    function getScreenWidth() {
+        return Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    }
+
+    function getScreenHeight() {
+        return Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    }
+
+    function assureLandscape() {
+        if (this.width < this.height) {
+            var newWidth = this.height;
+            this.height = this.width;
+            this.width = newWidth;
+        }
+    }
+}
