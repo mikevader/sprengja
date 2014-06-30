683cd74a80e3166f942d0fe28f72bd71b72dca7f
Matthias Baumeler <matthias.baumeler@gmail.com>
2014-06-30 16:53:07 +0200

    Remove html buttons and replace with canvas buttons.

--- public/menu.js
+++ public/menu.js
@@ -1,28 +1,46 @@
+/*global game, console */
 var Sprengja = Sprengja || {};
 Sprengja.Menu = {
 
-    show : function() {
+    show : function () {
+    
+        var newClientButton,
+            newServerButton,
+            buttons;
+        
+        function hide() {
+            var i = 0;
+        
+            for (i = 0; i < buttons.length; i += 1) {
+                buttons[i].visible = false;
+            }
+        }
+        
+        function startNewLocalGame() {
+            console.log('Start local game');
+            game.state.getCurrentState().initialized = false;
+            game.state.getCurrentState().session = null;
+            game.state.getCurrentState().initGame();
 
-        function initButton(resource, callBackFunction) {
-            var image = game.add.sprite(game.world.centerX, game.world.centerY, resource);
+            hide();
+        }
+    
+        function startNewServerGame() {
+            game.state.getCurrentState().initialized = false;
+            game.state.getCurrentState().session = null;
+            game.state.getCurrentState().initRemoteGame();
+        }
+        
+        function initButton(resource, callBackFunction, y) {
+            var image = game.add.sprite(game.world.centerX, y, resource);
             image.anchor.set(0.5);
             image.inputEnabled = true;
             image.events.onInputDown.add(callBackFunction, this);
+            return image;
         }
         
-        initButton(Sprengja.Resources.NEW_CLIENT_GAME, Sprengja.Menu.startNewLocalGame);
-        initButton(Sprengja.Resources.NEW_SERVER_GAME, Sprengja.Menu.startNewServerGame);
-    },
-    
-    hide : function() {
-    
-    },
-    
-    startNewLocalGame : function() {
-        console.log('NEUES GAME');
-    },
-    
-    startNewServerGame : function() {
-    
+        newClientButton = initButton(Sprengja.Resources.NEW_CLIENT_GAME, startNewLocalGame, game.world.centerY * 0.3);
+        newServerButton = initButton(Sprengja.Resources.NEW_SERVER_GAME, startNewServerGame, game.world.centerY);
+        buttons = [newClientButton, newServerButton];
     }
-}
+};
