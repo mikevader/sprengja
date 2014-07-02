68931ca03b3af71cc2dc03407df23e0680490500
Nicolas Oeschger <noe@exq.ch>
2014-07-02 11:01:40 +0200

    Removed TODO for speed ratio

--- public/js/game.js
+++ public/js/game.js
@@ -332,7 +332,6 @@ GameState.prototype.update = function() {
 
                 if (this.game.input.activePointer.isDown) {
                     // Trigger is still down
-                    // TODO nioe: Draw bullet speed ratio on gui
                     Sprengja.Menu.text.setCenteredText(Math.floor(bulletSpeedRatio * 100) + '%');
                 } else {
                     // Player released trigger
