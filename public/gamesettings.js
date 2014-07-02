ee0929fcbfb83959a4ab2a6fbfef87c435c17b82
Nicolas Oeschger <noe@exq.ch>
2014-07-02 09:27:41 +0200

    Increased bullet speed

--- public/gamesettings.js
+++ public/gamesettings.js
@@ -3,8 +3,8 @@ var Sprengja = Sprengja || {};
 Sprengja.Settings = {
     
     SHOT_DELAY : 300,           // milliseconds (10 bullets/3 seconds)
-    MAX_TRIGGER_DOWNTIME: 5000,
-    BULLET_SPEED : 0.7,         // Relative to screen size (pixels/second)
+    MAX_TRIGGER_DOWNTIME: 1500,
+    BULLET_SPEED : 1.5,         // Relative to screen size (pixels/second)
     NUMBER_OF_BULLETS : 20,
     GRAVITY : 980,               // pixels/second/second
     BACKGROUND_COLOR : 0x4488cc,
